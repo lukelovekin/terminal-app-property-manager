ec4237bb17437cf6cd3ef5992a6c308bb200d86d
Luke <lukelovekin@gmail.com>
2020-03-13 21:23:30 +1000

    "Last and final"

--- src/test.rb
+++ src/test.rb
@@ -1,49 +1,24 @@
 # require "test/unit"
-# require_relative "property_manager_tenant.rb"
-
-# class PropertyTest < Test::Unit:Textcase
-
-# def test_options
-#     item1= Item.new({name:"test",})
-
-# end
-
-# end
-
-   
-#          item1 = Item.new({name:"test", price:1})
-#          assert_equal(item1.start_sale, "a customer walks in, They want some test")
-#      end
-#  end
+# require_relative "./property_manager_tenant.rb"
 
 # class TestItem < Test::Unit::TestCase
-#    def test_greet_customer
-#        customer1=Customer.new({
-#            customer:"test",
-#             customer:"test",
-#             customer:"test",
-#             customer:"test"
-#            }, Item.new({name:"test", price:1})
-#        assert_equal(customer1.greet_customer, "hello test i hear you're after pair of test great choice ")
+#     def property_2
+#     #    customer1=Customer.new({
+#     #        customer:"test",
+#     #         customer:"test",
+#     #         customer:"test",
+#     #         customer:"test"
+#     #        }, Item.new({name:"test", price:1})
+#     #    assert_equal(customer1.greet_customer, "")
 #    end
-# end
 
-
-
-
-
-# def divide(num1, num2)
-#     num1/num2
-# end
-
-# begin
-#     divide(4, 2) # All good no error
-#     divide(4, 0) # Zero division error
-#     divide(4, " ") # Type error
-# rescue ZeroDivisionError
-#     puts "Zero Division error: Cannot divide a number by 0"
-# rescue TypeError
-#     puts "Type Error: number cannot be a string"
-# rescue
-#     puts "some other standard Error handled"
+#   def property_2
+#     #    customer1=Customer.new({
+#     #        customer:"test",
+#     #         customer:"test",
+#     #         customer:"test",
+#     #         customer:"test"
+#     #        }, Item.new({name:"test", price:1})
+#     #    assert_equal(customer1.greet_customer, "")
+#    end
 # end
